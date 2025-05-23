a39af62dff120496511a5c35ab79f55789b6aef9
tboyobi <obitony707@gmail.com>
2025-05-23 14:43:15 +0100

    feat: add initial server setup with Express and Axios

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
