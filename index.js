f96e31682da47bf3b3e1a5c21cf852a3eb15fb3c
tboyobi <obitony707@gmail.com>
2025-05-24 13:30:58 +0100

    feat: implement anime info retrieval and update index view with details display

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ import axios from "axios";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const base_URL = "https://api.animechan.io/v1/anime";
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-
+app.post("/name", async (req, res) => {
+  const { name } = req.body;
+  try {
+    const response = await axios.get(`${base_URL}/${name}`);
+    const anime = response.data;
+    res.render("index.ejs", {
+      name: anime.name,
+      episode: anime.episode,
+      summary: anime.summary,
+      id: anime.id
+    });
+  } catch (error) {
+    console.error("Error fetching quote:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
