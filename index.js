f359d6e6c4bf2f99ca268d2a32c065a4d02b1246
tboyobi <obitony707@gmail.com>
2025-05-24 10:14:49 +0100

    feat: remove recommendation route and associated form from index view

--- index.js
+++ index.js
@@ -9,24 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-app.post("/recommendation", async (req, res) => {
-  const { id } = req.body;
-  try {
-    const response = await axios.get(
-      `https://api.jikan.moe/v4/recommendations/${id}`, {
-        params: {
-          limit: 10
-        }
-      }
-    );
-    let recommendations = response.data;
 
-    res.render("index.ejs", { name: recommendations });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error fetching anime details");
-  }
-});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
